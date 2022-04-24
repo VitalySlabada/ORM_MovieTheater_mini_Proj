cccfcb2ee1205484c62e62f9ae58f446165a3ff0
VitalySlabada <vitaly.slabada@gmail.com>
2022-04-24 14:55:04 -0500

    Initial commit

---
+++ src/main/java/com/orm_movietheater_mini_proj/OrmMovieTheaterMiniProjApplication.java
@@ -0,0 +1,13 @@
+package com.orm_movietheater_mini_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmMovieTheaterMiniProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmMovieTheaterMiniProjApplication.class, args);
+    }
+
+}
